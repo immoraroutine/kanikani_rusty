5233d7f68ae8ba228be67df11ee0f38157fb6acc
immoraroutine <immoraroutine@gmail.com>
2024-07-02 22:00:27 +0900

    guessing game!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Guess the number!");
+    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+           
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            },
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+        }
+    }
+
+    
 }
