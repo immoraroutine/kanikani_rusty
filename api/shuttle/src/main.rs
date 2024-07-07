505781272df9b9b63ad7a64cbf162675f3a1e15a
immoraroutine <immoraroutine@gmail.com>
2024-07-07 14:04:43 +0900

    add database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,11 +1,14 @@
 use axum::{routing::get, Router};
+use sqlx::PgPool;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
