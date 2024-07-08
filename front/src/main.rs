d549fd690fcb94e1ec5e437e609d4559694d668d
immoraroutine <immoraroutine@gmail.com>
2024-07-09 04:10:48 +0900

    add reusable component

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(non_snake_case)]
 // Import the Dioxus prelude to gain access to the `rsx!` macro and the `Scope` and `Element` types.
 mod components;
+mod models;
 
-use components::{Footer, Header};
+use components::{FilmModal, Footer, Header};
 use dioxus::prelude::*;
-
 
 fn main() {
     // Launch the web application using the App component as the root.
@@ -14,16 +14,20 @@ fn main() {
 // Define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
     cx.render(rsx! {
--       div {
--           "Hello, world!"
--       }
-+       main {
-+           class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
-+           Header {}
-+           section {
-+               class: "md:container md:mx-auto md:py-8 flex-1",
-+           }
-+           Footer {}
-+       }
+       div {
+           "Hello, world!"
+       }
+       main {
+           class: "relative z-0 bg-blue-100 w-screen h-auto min-h-screen flex flex-col justify-start items-stretch",
+           Header {}
+           section {
+               class: "md:container md:mx-auto md:py-8 flex-1",
+           }
+            Footer {}
+            FilmModal {
+                on_create_or_update: move |_| {},
+                on_cancel: move |_| {},
+           }
+       }
     })
 }
